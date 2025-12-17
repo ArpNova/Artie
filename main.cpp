7628d1fbf2145b4b37cbd16332b033da102a1fb4
ArpNova <arpanbiswas.myworld@gmail.com>
2025-12-17 19:02:25 +0530

    feat: Implement a moving and draggable sprite with drag functionality and screen edge collision, and ignore the assets directory.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,90 @@
 #include <QApplication>
-#include <QWidget>
+#include <QMouseEvent>
 #include <QPainter>
 #include <QTimer>
-#include <QMouseEvent>
+#include <QWidget>
+#include <qpixmap.h>
 
 class Mate : public QWidget {
+private:
+  QPixmap sprite;
+  QTimer *timer;
+  int x_velocity = 2;
+  QPoint dragPosition;
+  bool isDragging = false;
+
 public:
-    Mate(){
-        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
+  Mate(QWidget *parent = nullptr) : QWidget(parent) {
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint |
+                   Qt::Tool);
 
-        setAttribute(Qt::WA_TranslucentBackground);
+    setAttribute(Qt::WA_TranslucentBackground);
 
-        resize(200, 200);
+    bool success = sprite.load("./assets/Eren.png");
+
+    if (success) {
+      resize(sprite.size());
+    } else {
+      resize(100, 100);
     }
+
+    timer = new QTimer(this);
+
+    connect(timer, &QTimer::timeout, this, &Mate::updatePosition);
+
+    timer->start(30);
+  }
+
+  void updatePosition() {
+    int currentX = this->x();
+    int currentY = this->y();
+
+    int newX = currentX + x_velocity;
+
+    // screen collision
+    if (newX > 1080) {
+      x_velocity = -2;
+    }
+    if (newX < 0) {
+      x_velocity = 2;
+    }
+
+    this->move(newX, currentY);
+  }
+
 protected:
-    void paintEvent(QPaintEvent *event)override{
-        QPainter painter(this);
+  void mousePressEvent(QMouseEvent *event) override {
+    if (event->button() == Qt::LeftButton) {
+      isDragging = true;
 
-        painter.setBrush(Qt::blue);
-        painter.drawEllipse(0, 0, 100, 100);
+      dragPosition =
+          event->globalPosition().toPoint() - frameGeometry().topLeft();
+      event->accept();
     }
+  }
+
+  void mouseMoveEvent(QMouseEvent *event) override {
+    if (event->buttons() & Qt::LeftButton && isDragging) {
+      move(event->globalPosition().toPoint() - dragPosition);
+      event->accept();
+    }
+  }
+
+  void mouseReleaseEvent(QMouseEvent *event) override { isDragging = false; }
+
+  void paintEvent(QPaintEvent *event) override {
+    QPainter painter(this);
+
+    painter.drawPixmap(0, 0, sprite);
+  }
 };
 
-int main(int argc, char *argv[]){
-    QApplication app(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
 
-    Mate myMate;
+  Mate myMate;
 
-    myMate.show();
+  myMate.show();
 
-    return app.exec();
+  return app.exec();
 }
